574e2e5579b82ff55b57a3e69c3c4a5fd9c8e967
Brian Hoerner <BrianHoerner87@gmail.com>
2021-05-03 11:18:33 -0400

    Initial commit. Or nearly initial. I have added my opening comments declaring what I intend to do with the application.

--- Week16_Final_Pantry/main.cpp
+++ Week16_Final_Pantry/main.cpp
@@ -1,9 +1,11 @@
-//
-//  main.cpp
-//  Week16_Final_Pantry
-//
-//  Created by Brian Hoerner on 5/3/21.
-//
+// Brian Hoerner
+// CIS 1202.501
+// 3 May 2021
+// This Pantry Application will allow the user to keep track of his or her
+// kitchen items and check their expiration dates against the current date
+// to identify products which should be discarded.
+// In a future version I would like the application to be able to suggest
+// potential meals from available food items.
 
 #include <iostream>
 
