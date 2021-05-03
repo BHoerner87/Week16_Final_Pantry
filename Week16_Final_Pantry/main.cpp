3edbb929472604bcf8ee10e95a4313be62f21978
Brian Hoerner <BrianHoerner87@gmail.com>
2021-05-03 11:12:51 -0400

    Initial Commit

---
+++ Week16_Final_Pantry/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Week16_Final_Pantry
+//
+//  Created by Brian Hoerner on 5/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
