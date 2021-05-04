82e4a6616aecb506a549a175b860d3fae6d9dd9f
BHoerner87 <71803061+BHoerner87@users.noreply.github.com>
2021-05-04 15:43:19 -0400

    Update Food.cpp
    Updating all files. Realized a function I had been writing into Food didn't belong there and put it in main for the time being. It's likely going to be moved into the Pantry class.

--- Week16_Final_Pantry/Food.cpp
+++ Week16_Final_Pantry/Food.cpp
@@ -9,19 +9,3 @@
 
 using namespace std;
 
-void addItem()
-{
-  // Create new instance
-  Food *foodItem = new Food;
-  // Set name
-//    cout << "\nWhat is the name of the item you are adding? ";
-//    string tmpName = "";
-//    getline(cin, tmpName);
-//    setName(tmpName);
-//    cout << "\n"
-  // Call Expiration setter function or something I don't know
-  // Set quantity
-  // Probably package all this stogether, eh? Or no, it should
-  // probably be menu-driven so you can overwrite / update things.
-
-}
