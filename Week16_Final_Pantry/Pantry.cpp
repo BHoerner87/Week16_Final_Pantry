3bbd04ebb7ff8853332e8b77f96d1c7338d1747f
BHoerner87 <71803061+BHoerner87@users.noreply.github.com>
2021-05-04 15:32:04 -0400

    Update Pantry.cpp
    Added a function for changing the month name to a string, am developing the function to delete an item from the vector of pantry food.

--- Week16_Final_Pantry/Pantry.cpp
+++ Week16_Final_Pantry/Pantry.cpp
@@ -4,13 +4,51 @@
 //
 //  Created by Brian Hoerner on 5/3/21.
 //
+#include <iomanip>
 #include <iostream>
 #include "Pantry.h"
 
 using namespace std;
 
-Food Pantry::addFood()
+void Pantry::tossFood()
 {
-  Food tempFood;
-  cout << "What kind of item are you adding?";
+  // List expired
+	//  using for loop
+	if (!(storage.size() > 0))
+		return;
+	for (int i = 0; i < storage.size(); i++)
+	{
+		if (storage[i].expired == true)
+			cout << (i + 1) << ". " << storage[i].name << endl
+					 << "\texpired on " << monthName(Pantry::expir.getmonth())
+					 << ' ' << Pantry::expir.getDay() << ", "
+					 << Pantry::expir.getYear() << endl;
+	}
+	// Prompt user
+	cout << "\nWhich item would you like to toss out?"
+			 << "\nChoose a number from the list above "
+			 << "(1 - " << storage.size() << 
+	// Get user response
+	// Are you sure?
+	// Delete vector element
+	// Loop until quit
+}
+
+string Pantry::monthName(int m)
+{
+	switch (m)
+	{
+		case 1: return "January ";		// I believe I'm right in not
+		case 2: return "February ";		// including "break" because
+		case 3: return "March ";			// "return" will leave the function
+		case 4: return "April ";
+		case 5: return "May ";
+		case 6: return "June ";
+		case 7: return "July ";
+		case 8: return "August ";
+		case 9: return "September ";
+		case 10: return "October ";
+		case 11: return "November ";
+		case 12: return "December ";
+	}
 }
