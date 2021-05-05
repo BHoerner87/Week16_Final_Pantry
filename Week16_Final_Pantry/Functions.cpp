cb362de0207502783110c69f1c2eced8b339bbd5
Brian Hoerner <BrianHoerner87@gmail.com>
2021-05-05 07:22:51 -0400

    Created a functions header and source file to try and get functions out of main and keep it clean and readable.

---
+++ Week16_Final_Pantry/Functions.cpp
@@ -0,0 +1,8 @@
+//
+//  Functions.cpp
+//  Week16_Final_Pantry
+//
+//  Created by Brian Hoerner on 5/5/21.
+//
+
+#include "Functions.h"
---
+++ Week16_Final_Pantry/Functions.h
@@ -0,0 +1,13 @@
+//
+//  Functions.hpp
+//  Week16_Final_Pantry
+//
+//  Created by Brian Hoerner on 5/5/21.
+//
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdio.h>
+
+#endif /* Functions_hpp */
