cb7f89a71f5082a38635ffbe4462ec4a228ed0c0
Brian Hoerner <BrianHoerner87@gmail.com>
2021-05-07 11:47:31 -0400

    I'm beginning the great paring down of features and burying of old data in classes.

---
+++ Week16_Final_Pantry/Calendar.cpp
@@ -0,0 +1,104 @@
+//
+//  Calendar.cpp
+//  Week16_Final_Pantry
+//
+//  Created by Brian Hoerner on 5/7/21.
+//
+
+#include "Calendar.h"
+
+#include "Calendar.h"
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+void Calendar::setYear()
+{
+  int tempInt;
+  cout << "\nWhat is the current year? ";
+  while (!(cin >> tempInt) || tempInt < 2021 || tempInt > 2099)
+  {
+    cin.clear();
+    cin.ignore(1000, '\n');
+    cout << "\nPlease enter a valid year: ";
+  }
+  year = tempInt;
+  cout << "\nYear set!";
+  return;
+}
+
+void Calendar::setMonth()
+{
+  int tempInt;
+  cout << '\n' << setw(20) << left << "1. January" << setw(20) << "2. February" << setw(20) << "3. March" << setw(20) << "4. April" << endl
+           << setw(20) << "5. May" << setw(20) << "6. June" << setw(20) << "7. July" << setw(20) << "8. August" << endl
+           << setw(20) << "9. September" << setw(20) << "10. October" << setw(20) << "11. November" << setw(20) << "12. December"
+           << endl;
+  cout << "\nPlease enter the current month: ";
+  while (!(cin >> tempInt) || tempInt < 1 || tempInt > 12)
+  {
+    cin.clear();
+    cin.ignore(1000, '\n');
+    cout << "\nPlease enter a valid month: ";
+  }
+  month = tempInt;
+  cout << "\nMonth set!";
+  return;
+}
+
+void Calendar::setDay()
+{
+  int tempInt;
+  cout << "\nWhat day of the month is it today? ";
+  if (Calendar::month == 2)
+  while (!(cin >> tempInt || tempInt < 1 || tempInt > 28))
+    {
+      std::cin.clear();
+      std::cin.ignore(1000, '\n');
+      std::cout << "\nPlease make a valid entry: ";
+    }
+  else if (Calendar::month == 4 || Calendar::month == 6 || Calendar::month == 9 || Calendar::month == 11)
+  {
+    while (!(cin >> tempInt) || tempInt < 1 || tempInt > 30)
+    {
+      std::cin.clear();
+      std::cin.ignore(1000, '\n');
+      std::cout << "\nPlease make a valid entry: ";
+    }
+    else
+    {
+      while (!(cin >> tempInt) || tempInt < 1 || tempInt > 31)
+      {
+        std::cin.clear();
+        std::cin.ignore(1000, '\n');
+        std::cout << "\nPlease make a valid entry: ";
+      }
+    }
+    day = tempInt;
+    cout << "\nDay set!";
+    return;
+  }
+}
+
+void Expiration::setExYear()
+{
+  int tempInt;
+  cout << "\nWhat is the expiration year? ";
+  while (!(cin >> tempInt) || tempInt < 2021 || tempInt > (Calendar::getYear()+15))
+  {
+    cin.clear();
+    cin.ignore(1000, '\n');
+    cout << "\nPlease enter a valid year: ";
+  }
+}
+
+void Expiration::setExMonth()
+{
+
+}
+
+void Expiration::setExDay()
+{
+
+}
---
+++ Week16_Final_Pantry/Calendar.h
@@ -0,0 +1,48 @@
+#ifndef CALENDAR_H
+#define CALENDAR_H
+
+// Calendar replaces the old Date class but will do the same thing.
+// Calendar should only be kept as an instance on (in) the Whiteboard class
+// Calendar's derived class, Expiration, will exist inside the Food class
+
+class Calendar
+{
+private:
+  // Today's date
+  static int year;
+  static int month;
+  static int day;
+
+public:
+  // Setters;
+  void setYear(int y)
+  { year = y; }
+  static void setYear();
+  void setMonth(int m)
+  { month = m; }
+  static void setMonth();
+  void setDay(int d)
+  { day = d; }
+  static void setDay();
+
+  // Getters
+  int getYear()
+  { return year; }
+  int getMonth()
+  { return month; }
+  int getDay()
+  { return day; }
+
+  // Other methods
+};
+
+class Expiration : public Calendar
+{
+private:
+public:
+  void setExYear();
+  void setExMonth();
+  void setExDay();
+};
+
+#endif
